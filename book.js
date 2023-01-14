7bb60159de9698cca09c00ea5aa94d80620ffec0
McWooden <garryf7887@gmail.com>
2023-01-14 22:59:06 +0700

    menambahkan router book dan mengubah schema book

--- book.js
+++ book.js
@@ -2,68 +2,143 @@ import express from 'express'
 const router = express.Router()
 import { Book } from './schema.js'
 router.get('/', (req, res) => {
+    Book.find({}).select('profile').exec((err, books) => {
+        if(err) {
+            return res.status(500).send(err)
+        }
+        res.send(books)
+    })
+})
+
+router.get('/', (req, res) => {
     Book.find({}, (err, books) => {
         res.send(books)
     })
 })
+// get rooms
+router.get('/:bookId/get/pages/details', (req, res) => {
+    Book.findById(req.params.bookId).select('pages.details').exec((err, pages) => {
+        if(err) {
+            return res.status(500).send(err)
+        }
+        res.send(pages)
+    })
+})
+// get list
+router.get('/:bookId/get/pages/list', (req, res) => {
+    Book.findById(req.params.bookId).select('pages.list').exec((err, pages) => {
+        if(err) {
+            return res.status(500).send(err)
+        }
+        res.send(pages)
+    })
+})
+// add
+router.get('/:bookId/add/page', async (req, res) => {
+    try {
+        const updatedDocument = await Book.findOneAndUpdate(
+            { _id: req.params.bookId },
+            { $push: { 'pages': {
+                details: {
+                    page_title: 'Page Test',
+                    icon: 'faCheck',
+                    jadwal_url: 'https://source.unsplash.com/random/test',
+                },
+                list: [{
+                    details: {
+                        item_title: 'First list',
+                        desc: 'ini adalah list pertamamu',
+                        color: 'yellowgreen',
+                        deadline: '12/31/2022'
+                    },
+                    dones: [],
+                    notes: [{
+                        context: 'catatan tentang list berada disini',
+                        by: 'developer',
+                        date: '12/30/2022',
+                        color: 'royalblue'
+                    }],
+                    images: [{
+                        pic: 'https://source.unsplash.com/random/introduce',
+                        desc: 'gambar disimpan disini',
+                        date: '12/30/2022',
+                        by: 'developer'
+                    }],
+                    chat: [{
+                        nickname: 'developer',
+                        msg: 'kamu bisa membahas list disini',
+                        time: '22.50',
+                        date: '12/30/2022'
+                    }]
+                }]
+            } } },
+            { new: true }
+        )
+        res.json(updatedDocument)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
 router.get('/addBook', (req, res) => {
     let book = new Book({
         profile: {
-            book_title: 'buku udin',
-            avatar_url: 'https://source.unsplash.com/random/bukuudin',
+            book_title: 'Buku Huddin',
+            avatar_url: 'https://source.unsplash.com/random/buku panduan',
             author: {
-                nickname: 'McWooden',
-                tag: '2521'
+                nickname: 'Huddin',
+                tag: '2145'
             },
         },
         pages: [{
-            page_title: 'Welcome',
-            icon: 'faAddressBook',
-            jadwal_url: 'https://source.unsplash.com/random/Welcome',
-            list: [
-                {
-                    title: 'First list',
+            details: {
+                page_title: 'Tugas harian',
+                icon: 'faAddressBook',
+                jadwal_url: 'https://source.unsplash.com/random/Welcome-buku panduan',
+            },
+            list: [{
+                details: {
+                    item_title: 'First list',
                     desc: 'ini adalah list pertamamu',
-                    list_color: 'yellowgreen',
-                    user_done: [],
-                    deadline: '12/31/2022',
-                    chat: [
-                        {
-                            nickname: 'Book',
-                            msg: 'kamu bisa membahas list disini',
-                            time: '22.50',
-                            date: '12/30/2022'
-                        }
-                    ],
-                    notes: [
-                        {
-                            context: 'catatan tentang list berada disini',
-                            by: 'pengenalan',
-                            date: '12/30/2022',
-                            color: 'royalblue'
-                        },
-                    ],
-                    images: [
-                        {
-                            pic: 'https://source.unsplash.com/random/introduce',
-                            desc: 'gambar disimpan disini',
-                            date: '12/30/2022',
-                            by: 'Book'
-                        },
-                    ]
-                }
-            ]
+                    color: 'yellowgreen',
+                    deadline: '12/31/2022'
+                },
+                dones: [],
+                notes: [{
+                    context: 'catatan tentang list berada disini',
+                    by: 'developer',
+                    date: '12/30/2022',
+                    color: 'royalblue'
+                }],
+                images: [{
+                    pic: 'https://source.unsplash.com/random/introduce',
+                    desc: 'gambar disimpan disini',
+                    date: '12/30/2022',
+                    by: 'developer'
+                }],
+                chat: [{
+                    nickname: 'Mimo',
+                    msg: 'kamu bisa membahas list disini',
+                    time: '22.50',
+                    date: '12/30/2022'
+                }]
+            }]
         }],
         users: [{
-            role: 'Admin',
-            role_color: 'goldenrod',
-            role_member: [
-                {name: 'McWooden', tag: '2521', pic: 'https://source.unsplash.com/random/McWooden', status: 'ngoding'},
-            ]
-        }]
+            details: {
+                role: 'Admin',
+                role_color: 'goldenrod'
+            },
+            member: [{
+                nickname: 'Huddin', 
+                tag: '4235', 
+                avatar: 'https://source.unsplash.com/random/McWooden', 
+                status: 'learning'
+            }]
+        }],
     })
     book.save()
     console.log(book)
     res.send('buku baru dibuat')
 })
+
 export default router
--- schema.js
+++ schema.js
@@ -8,8 +8,52 @@ export const Book = mongoose.model('books', {
             tag: String
         },
     },
-    pages: [Object],
-    users: [Object],
+    pages: [{
+        details: {
+            page_title: String,
+            icon: String,
+            jadwal_url: String,
+        },
+        list: [{
+            details: {
+                item_title: String,
+                desc: String,
+                color: String,
+                deadline: String
+            },
+            dones: [String],
+            notes: [{
+                context: String,
+                by: String,
+                date: String,
+                color: String
+            }],
+            images: [{
+                pic: String,
+                desc: String,
+                date: String,
+                by: String
+            }],
+            chat: [{
+                nickname: String,
+                msg: String,
+                time: String,
+                date: String
+            }]
+        }] 
+    }],
+    users: [{
+        details: {
+            role: String,
+            role_color: String
+        },
+        member: [{
+            nickname: String, 
+            tag: String, 
+            avatar: String, 
+            status: String
+        }]
+    }]
 })
 export const User = mongoose.model('users', {
     name: String,
