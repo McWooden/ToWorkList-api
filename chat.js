352c454c706f6c5e9c5c0e94bbef2f7955988f8c
McWooden <garryf7887@gmail.com>
2023-07-23 13:20:37 +0700

    send new item chat res

--- chat.js
+++ chat.js
@@ -4,35 +4,44 @@ import { Book } from './schema.js'
 
 router.post('/:pageId/:listId', (req, res) => {
     const query = {
-        'pages': {$elemMatch: { _id: req.params.pageId, 'list': {$elemMatch: { _id: req.params.listId }}}}
+        'pages': { $elemMatch: { _id: req.params.pageId, 'list': { $elemMatch: { _id: req.params.listId } } } }
     }
-    const update = {
-        $push: {
-            'pages.$[page].list.$[list].chat': {
-                nickname: req.body.nickname,
-                msg: req.body.msg,
-                date: +new Date()
+
+    const newItem = {
+        nickname: req.body.nickname,
+        msg: req.body.msg,
+        date: +new Date()
+    }
+
+    Book.findOne(query)
+        .then(book => {
+            if (!book) {
+                return res.status(404).json({ success: false, error: 'Page or List not found' })
             }
-        }
-    }
-    const options = { 
-        new: true,
-        arrayFilters: [{ 'page._id': req.params.pageId }, { 'list._id': req.params.listId }]
-    }
-    Book.findOneAndUpdate(query, update, options)
-    .then(result => {
-        if (result) {
-            const page = result.pages.find(obj => obj._id.toString() === req.params.pageId)
+
+            const page = book.pages.find(obj => obj._id.toString() === req.params.pageId)
             const list = page.list.find(obj => obj._id.toString() === req.params.listId)
-            res.json({ success: true, data: list })
-        } else {
-            res.status(404).json({ success: false, error: 'Page or List not found' })
-        }
-    }).catch(err => {
-        console.log(err)
-        res.status(404).json({ success: false, error: err })
-    })
+            const chatArray = list.chat
+
+            if (chatArray.length >= 20) {
+                chatArray.shift()
+            }
+
+            chatArray.push(newItem)
+
+            return book.save()
+        })
+        .then(savedBook => {
+            const page = savedBook.pages.find(obj => obj._id.toString() === req.params.pageId)
+            const list = page.list.find(obj => obj._id.toString() === req.params.listId)
+            res.json({ success: true, data: list, new: newItem })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(404).json({ success: false, error: err })
+        })
 })
+
 router.put('/:pageId/:listId/:chatId', (req, res) => {
     const query = {
         'pages': {$elemMatch: { _id: req.params.pageId, 'list': {$elemMatch: { _id: req.params.listId, 'chat': {$elemMatch: {_id: req.params.chatId}}}}}}
