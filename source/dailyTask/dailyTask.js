76dea58fdb5606d4f1241678351c076f90b930a6
McWooden <garryf7887@gmail.com>
2023-08-26 11:16:35 +0700

    lupa blo di psh reverse daily

--- source/dailyTask/dailyTask.js
+++ source/dailyTask/dailyTask.js
@@ -13,6 +13,54 @@ router.get('/all', async (req, res) => {
     }
 })
 
+router.put('/reverse/:taskId/:listId', async (req, res) => {
+    try {
+        const daily = await DailyTask.findById(req.params.taskId)
+        if (!daily) return res.status(404).json({ error: 'Task not found' })
+
+        const list = daily.list.id(req.params.listId)
+        if (!list) return res.status(404).json({ error: 'List not found' })
+
+        const userId = req.body._id
+        const userExists = list.check.includes(userId)
+
+        if (userExists) {
+            list.check.remove(userId)
+        } else {
+            list.check.push(userId)
+        }
+
+        await daily.save()
+        res.json({ task: daily })
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ error: 'Internal server error' })
+    }
+})
+
+router.post('/follow/:dailyTaskId', (req, res) => {
+    const newFollowers = req.body
+    DailyTask.findById(req.params.dailyTaskId, (err, daily) => {
+        if (err) return res.send('Tugas harian tidak ditemukan)')
+        const userExists = daily.followers.some(user => user._id === newFollowers._id)
+        if (userExists) return res.json({message: 'Anda sudah mengikuti'})
+        const update = {
+            $addToSet: {
+                'followers': {
+                    name: newFollowers.nickname,
+                    avatar: newFollowers.avatar, 
+                    _id: newFollowers._id,
+                }
+            }
+        }
+        const options = {new: true}
+        DailyTask.findByIdAndUpdate(req.params.dailyTaskId, update, options, (err, updatedDaily) => {
+            if (err) res.send('Kesalahan pada update)')
+            res.json({message: 'Anda sudah terdaftar, silahkan segarkan tugas sekarang!'})
+        })
+    })
+})
+
 router.get('/createOne', async (req, res) => {
     const data = new DailyTask({
         detail: {
